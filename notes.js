541fc62ac22463921d73294ff00bd00ead4ccaa6
Johann Rajajakrunanayake <rajakarunanayakejohann@yahoo.co.uk>
2021-09-20 22:27:31 +0100

    new timer returns score

--- notes.js
+++ notes.js
@@ -19,7 +19,7 @@ function createImage(notesArray) {
     document.getElementById("image").appendChild(newImage);
 }
 
-function changeImage(notesArray){
+function changeImage(notesArray) {
     if (document.getElementById("image").childNodes.length <= 1) {
         createImage(notesArray);
     }
@@ -30,14 +30,24 @@ function changeImage(notesArray){
     }
 }
 
-function countScore(id, totalScore){
+function countScore(totalScore) {
 
-    if(id === 'tick-button'){
+    if (id === 'tick-button') {
         totalScore++
-        return document.getElementById("score-panel").innerHTML = totalScore
+        document.getElementById("score-panel").innerHTML = totalScore
     }
 }
 
+function clickNewButton(newButton, id, totalScore, notesArray){
+    newButton.onclick = function () {
+        if (id === 'tick-button') {
+            totalScore++
+            document.getElementById("score-panel").innerHTML = "Score: " + totalScore
+        }
+        changeImage(notesArray);
+    }
+
+}
 
 function createButton(id, text, notesArray) {
     let newButton = document.createElement("BUTTON");
@@ -47,46 +57,69 @@ function createButton(id, text, notesArray) {
     newButton.setAttribute("width", "100");
 
     let totalScore = 0
-    newButton.onclick = function(){
-        
+    clickNewButton(newButton, id, totalScore, notesArray)
 
-        if(id === 'tick-button'){
-            totalScore++
-            document.getElementById("score-panel").innerHTML = "Score: " + totalScore
-        }   
-
-        changeImage(notesArray);
-
-    }
-    
     document.getElementById("buttons").appendChild(newButton);
 }
 
-function hideStartButton(id){
+function hideStartButton(id) {
     let startButton = document.getElementById(id)
     startButton.parentNode.removeChild(startButton);
 
 }
 
-function startTimer(){
-    let sec = 60;
-    let timer = setInterval(function(){
+function styleTimer(id) {
+    let timerStyle = document.getElementById(id).style;
+    timerStyle.border = "solid 2px #000";
+    timerStyle.borderRadius = "50%";
+    timerStyle.maxHeight = "25px";
+    timerStyle.maxWidth = "25px";
+    timerStyle.padding = "5px";
+}
+
+function returnScore(scorePanel){
+    let score = scorePanel.split(' ')[1]
+    document.getElementById('page').innerHTML = "TIME\'S UP!!! You scored " + score + "!";
+}
+
+
+function endTimer(sec, timer){
+    if (sec <= 0) {
+        clearInterval(timer);
+        let scorePanel = document.getElementById("score-panel").innerHTML
+        returnScore(scorePanel);
+    }
+}
+function startTimer() {
+    let sec = 6;
+    let timer = setInterval(function () {
         sec--;
         document.getElementById('timer').innerHTML = sec;
-        if (sec < 0) {
-            clearInterval(timer);
-        }
-       
+        styleTimer('timer');
+
+    endTimer(sec, timer);
+
     }, 1000);
+}
+
+
+function updateScorePanel(id){
+    let scorePanel = document.getElementById("score-panel");
+    scorePanel.innerHTML = "Score: 0";
+    scorePanel.style.border = "solid 3px #000";
+    scorePanel.style.height = "50px";
+    scorePanel.style.width = "70px";
+    scorePanel.style.padding = "10px 10px";
 }
 
 function clickStartButton() {
     let notesArray = ['C.png', 'E.png', 'G.png'];
 
     document.getElementById('timer').innerHTML = 60
+    styleTimer('timer');
     startTimer()
-    
-    document.getElementById("score-panel").innerHTML = "Score: 0" 
+
+    updateScorePanel('score-panel')
     randomNote(notesArray);
     hideStartButton("start-button");
 
