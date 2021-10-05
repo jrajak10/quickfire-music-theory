3ad0cba5f660e53cd763fb2fe8ee5400ea230c67
Johann Rajajakrunanayake <rajakarunanayakejohann@yahoo.co.uk>
2021-10-05 18:28:49 +0100

    added back buttons

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function hideButton(){
     document.getElementById('grade-list').style.display = "none";
     document.getElementById('grade-one-topics').style.display = "flex";
 }
+
+function goBack(list, topics){
+    document.getElementById(list).style.display = "flex";
+    document.getElementById(topics).style.display = "none";
+}
